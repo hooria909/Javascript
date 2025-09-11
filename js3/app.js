317ba13c0bbad9671291b5ce37c6be539de422ff
hooria909 <hooriarameen2@gmail.com>
2025-09-11 03:40:31 -0700

    Add files via upload

---
+++ js3/app.js
@@ -0,0 +1,68 @@
+
+// Chapter 14 to 16
+// question 1
+// var studentNames = [];
+// question 2 similar to question 1
+// var studentNames = new Array();
+// question 3
+// var stringArray = ["apple", "banana", "cherry"]
+// question 4
+// var numberArray = [1, 2, 3, 4, 5];
+// question 5
+// var booleanArray = [true, false, true];
+// question 6
+// var mixedArray = ["apple", 1, true, "bannana", 2, false];
+// question 7
+//  var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. phil", "PH.D"]
+//  document.write("<h1>Qualifications:</h1>")
+//  document.write("<ol>")
+//  for (var i = 0; i < qualifications.length; i++) {
+//  document.write("<li>" + qualifications[i] + "</li>")
+//  }
+//  document.write("</ol>")
+// question 8
+// var studentNames = ["Michael", "John", "Tony"];
+// var studentScores = [320, 230, 480];
+// var totalMarks = 500;
+// for (var i = 0; i < studentName s.Length ; i++) {
+//   var percentage = (studentScores[i] / totalMarks) * 100;}
+// document.write("Score of " + studentNames[i] + " is " + studentScores[i] + ". percentage: " + percentage + "%<br>"):
+// question 9
+// var colorNames = ["red", "green", "blue"];
+// document.write(colorNames + "<br>");
+// var usercolor = prompt("Enter a color to add to the beginning:");
+// colorNames.unshift(usercolor);
+// document.write(colorNames + "<br>");
+// usercolor = prompt("Enter a color to add to the end:");
+// colorNames.push(usecolor);
+// document.write(colorNames + "<br>");
+// colorNames.unshift("yellow", "purple");
+// document.write(colorNames + "<br>");
+// colorNames.shift();
+// documennt.write(colorNames + "<br>");
+// colorNames.pop();
+// document.write(colorNames + "<br>");
+// var index = prompt("Enter the index to add a color:");
+// var usercolor = prompt("Enter a color to add:");
+// colorNames.splice(index, 0, usercolor);
+// document.write(colorNames + "<br>");
+// var index = prompt("Enter the index to delete color(s):");
+// var count = prompt("Enter the number of colors to delete:");
+// colorNames.splice(index, count);
+// document.write(colorNames + "<br>");
+// question 10
+// var studentScores = [320, 230,480, 120];
+// document.write("scores of students: " + studentScores + "<br>");
+// studentScores.sort();
+// document.write("ordered scores of students: " + studentScores + "<br>");
+// question 11
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+// document.write("cities list: <br>" + cities + "<br>");
+// var selectedcities = cities.slice(2,4);
+// document.write("selcted cities list: <br>" + selectedcities + "<br>");
+// question 12
+// var arr = ["This", "is", "my", "cat"];
+// document.write("array: <br>" + arr + "<br>");
+// var str = arr.join(" ");
+// document.write("string: <br>" + str + "<br>");
+// The End
