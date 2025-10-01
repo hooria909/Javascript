ee5b82c669e21959b679facc84e212ccedd15909
hooria909 <hooriarameen2@gmail.com>
2025-10-01 15:53:56 +0500

    Add files via upload

---
+++ js8/app.js
@@ -0,0 +1,36 @@
+// chapter 49-52
+
+// q1
+
+// function submitForm(){
+//     var username = document.getElementById("name").value;
+//     var fullName = document.getElementById("fullName").value;
+//     var userEmail = document.getElementById("email").value;
+//     var userPassword = document.getElementById("password").value;
+
+
+//      document.getElementById("information").innerHTML = `
+//         <h3>Form Data:</h3>
+//         <p><strong>Username:</strong> ${username}</p>
+//         <p><strong>Full Name:</strong> ${fullName}</p>
+//         <p><strong>Email:</strong> ${userEmail}</p>
+//         <p><strong>Password:</strong> ${userPassword}</p>
+//     `;
+// }
+
+// q2
+
+// function completePara(){
+//     var paraGraph = document.getElementById("para");
+//     var linkIng = document.getElementById("link");
+
+//     if (linkIng.innerText === "Read More"){
+//         paraGraph.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique sequi unde pariatur exercitationem, laudantium ipsa! Asperiores assumenda quaerat, consectetur labore nostrum tenetur veniam voluptatibus possimus sed tempore quia similique, voluptatem corporis eveniet minus eligendi magni ea. Ipsum sunt corporis dolor minima, dolore deserunt soluta qui."
+//         linkIng.innerText ="Read Less"
+//     }
+//     else{
+//         paraGraph.innerText = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, qui!"
+//         linkIng.innerText = "Read More"
+
+//     }
+// }
